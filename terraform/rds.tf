c5e3550a5e486f425279b04f72abca82a16093a6
Amelina123 <146894964+Amelina123@users.noreply.github.com>
2026-01-21 22:13:52 +0000

    feat: ingress

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,93 +1,100 @@
-# rds.tf - Updated for custom VPC
+# Get existing VPC and subnet information
+data "aws_vpc" "default" {
+    default = true
+}
+data "aws_subnets" "default" {
+    filter {
+        name    = "vpc-id"
+        values  = [data.aws_vpc.default.id]
+    }
+}
 
-# Security group for Lambda functions
+# security group for lambda functions
 resource "aws_security_group" "lambda_sg" {
-  name        = "${var.project_name}-lambda-sg-${var.environment}"
-  description = "Security group for Lambda functions"
-  vpc_id      = aws_vpc.etl_vpc.id
-  
-  # Allow outbound to RDS
-  egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]  # Allow within VPC
-  }
-  
-  # Allow outbound HTTPS for AWS services
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  # Allow outbound for VPC endpoints
-  # egress {
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   cidr_blocks = ["10.0.0.0/16"]
-  # }
-  # egress {
-  #   from_port   = 53
-  #   to_port     = 53
-  #   protocol    = "udp"
-  #   cidr_blocks = ["10.0.0.0/16"]
-  #   description = "Allow DNS resolution"
-  # }
-  
-  tags = {
-    Name  = "${var.project_name}-lambda-sg"
-    Stage = "ETL-Pipeline"
-  }
+    name    = "${var.project_name}-lambda-sg"
+    description  = "Security group for lambda functions in VPC"
+    vpc_id       = data.aws_vpc.default.id
+
+#     ingress {
+#     description = "Allow HTTPS (443) between Lambdas and Interface VPC Endpoints using the same SG"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     self        = true
+#   }
+
+    # Allow all outbound traffic
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags ={
+        Name  = "${var.project_name}-lambda-sg"
+        Stage = "Week3-Loading"
+    }
 }
 
-# Security group for RDS
+resource "aws_security_group" "endpoints_sg" {
+    name    = "${var.project_name}-endpoints-sg"
+    description  = "Security group for VPC endpoints"
+    vpc_id       = data.aws_vpc.default.id
+
+    # Allow HTTPS (443) traffic from lambda security group
+    ingress {
+        from_port       = 443
+        to_port         = 443
+        protocol        = "tcp"
+        security_groups = [aws_security_group.lambda_sg.id]
+    }
+
+    # Allow all outbound traffic
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags ={
+        Name  = "${var.project_name}-endpoints-sg"
+    }
+}
+
+# security group for RDS
 resource "aws_security_group" "rds_sg" {
-  name        = "${var.project_name}-rds-sg-${var.environment}"
-  description = "Security group for RDS PostgreSQL"
-  vpc_id      = aws_vpc.etl_vpc.id
-  
-  # Allow PostgreSQL from Lambda security group
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda_sg.id]
-  }
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["80.7.249.208/32"]  # YOUR IP ONLY
-    # Get your IP: curl ifconfig.me
-  }
-  
-  # Allow all outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  tags = {
-    Name  = "${var.project_name}-rds-sg"
-    Stage = "ETL-Pipeline"
-  }
+    name    = "${var.project_name}-rds-sg"
+    description  = "Security group for RDS PostgresSQL"
+    vpc_id       = data.aws_vpc.default.id
+    # Allow PostgresSQL traffic from lambda security group only
+    ingress {
+        from_port       = 5432
+        to_port         = 5432
+        protocol        = "tcp"
+        security_groups = [aws_security_group.lambda_sg.id]
+    }
+    # Allow all outbound traffic from RDS (for updates etc..)
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags ={
+        Name  = "${var.project_name}-rds-sg"
+        Stage = "Week3-Loading"
+    }
 }
-
-# RDS subnet group (using database subnets)
+# RDS subnet group
 resource "aws_db_subnet_group" "warehouse" {
-  name       = "${var.project_name}-dw-subnet-group-${var.environment}"
-  subnet_ids = [aws_subnet.db_a.id, aws_subnet.db_b.id]
-  
-  tags = {
-    Name  = "Data Warehouse Subnet Group"
-    Stage = "ETL-Pipeline"
-  }
+    name       = "${var.project_name}-dw-subnet-group"
+    subnet_ids = data.aws_subnets.default.ids
+    tags ={
+        Name  = "Data Warehouse Subnet Group"
+        Stage = "Week3-Loading"
+    }
 }
 
 # RDS PostgreSQL Instance
@@ -104,10 +111,10 @@ resource "aws_db_instance" "data_warehouse" {
   password = var.dw_db_password
   
   # VPC Configuration
-  db_subnet_group_name   = aws_db_subnet_group.warehouse.name
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  publicly_accessible    = false  # CRITICAL: Set to false
-  
+  publicly_accessible      = false
+  db_subnet_group_name     = aws_db_subnet_group.warehouse.name
+  vpc_security_group_ids   = [aws_security_group.rds_sg.id]
+
   # Backup and Maintenance
   backup_retention_period = var.rds_backup_retention
   backup_window           = "03:00-04:00"
@@ -132,42 +139,43 @@ resource "aws_db_instance" "data_warehouse" {
   }
 }
 
-# # Store RDS credentials in Secrets Manager
-# resource "aws_secretsmanager_secret" "dw_creds_2" {
-#   name        = "${var.project_name}/dw/${var.environment}-warehouse"
-#   description = "Data warehouse RDS credentials"
-  
-#   tags = {
-#     Stage       = "ETL-Pipeline"
-#     Environment = var.environment
-#     Database    = "warehouse"
-#   }
-# }
-# EXISTING warehouse secret (already in AWS)
-
-# resource "aws_secretsmanager_secret" "dw_creds" {
-#   name = "${var.project_name}/dw/${var.environment}"
-
-#   recovery_window_in_days = 0  # allow immediate recreate if needed
-
-#   tags = {
-#     Stage       = "ETL-Pipeline"
-#     Environment = var.environment
-#     Database    = "warehouse"
-#   }
-# }
-data "aws_secretsmanager_secret" "dw_creds" {
-  name = "${var.project_name}/dw/${var.environment}"
+# Store RDS credentials in secret manager
+resource "aws_secretsmanager_secret" "dw_creds" {
+    name = "${var.project_name}/dw/${var.environment}"
+    description = "Data warehouse RDS credentials"
+    tags ={
+        Stage       = "Week3-Loading"
+        Environment = var.environment
+        Database    = "warehouse"
+    }
 }
+
+
+# data "aws_secretsmanager_secret" "dw_creds" {
+#   name = "${var.project_name}/dw/${var.environment}"
+# }
+
+# resource "aws_secretsmanager_secret_version" "dw_creds_value" {
+#   secret_id = data.aws_secretsmanager_secret.dw_creds.id
+
+#   secret_string = jsonencode({
+#     host     = aws_db_instance.data_warehouse.address
+#     port     = aws_db_instance.data_warehouse.port
+#     database = aws_db_instance.data_warehouse.db_name
+#     username = var.dw_db_username
+#     password = var.dw_db_password
+#   })
+# }
 
 resource "aws_secretsmanager_secret_version" "dw_creds_value" {
-  secret_id = data.aws_secretsmanager_secret.dw_creds.id
-
-  secret_string = jsonencode({
-    host     = aws_db_instance.data_warehouse.address
-    port     = aws_db_instance.data_warehouse.port
-    database = aws_db_instance.data_warehouse.db_name
-    username = var.dw_db_username
-    password = var.dw_db_password
-  })
+    secret_id = aws_secretsmanager_secret.dw_creds.id
+    secret_string = jsonencode({
+        host      = aws_db_instance.data_warehouse.address
+        port      = aws_db_instance.data_warehouse.port
+        database  = aws_db_instance.data_warehouse.db_name
+        username  = aws_db_instance.data_warehouse.username
+        user  = aws_db_instance.data_warehouse.username
+        password  = aws_db_instance.data_warehouse.password
+    })
 }
+
